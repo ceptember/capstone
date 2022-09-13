dede5aba9ef6ae4abb918038f93efeaa5dfa6a6b
Christy Perozzi <christy.perozzi@gmail.com>
2022-09-13 11:41:27 -0400

    get route fallback

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  
 end
